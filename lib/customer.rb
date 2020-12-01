74f03e72231e3588ca3233c628cebf61737e15b1
Lesly Juarez <leslyj2@gmail.com>
2020-12-01 15:55:11 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,4 +17,15 @@ def new_meal(waiter, total, tip=0)
   Meal.new(waiter, self, total, tip)
 end 
 
+def meals 
+  Meal.all.select do |meal|
+    meal.customer == self
+  end 
+end 
+
+def waiters 
+  meals.map do |meal|
+    meal.waiter
+  end 
+end 
 
