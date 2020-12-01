ecc99d409d76ddaa80f4609382de24210489bc4a
Lesly Juarez <leslyj2@gmail.com>
2020-12-01 15:45:14 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,17 @@
 class Customer
+  attr_accessor :name, :assert_generates
   
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self 
 end
+
+def self.all
+  @@all
+end 
+
+end 
+
